ed09243ee70515753b87cd1bcb06534d6405240a
AndresCortesVictor <andres.cortes.victor@est.una.ac.cr>
2025-05-26 00:01:43 -0600

    Andres Cortes
    modularidad aplicada, vistas de game, tutorial y ajustes credas mas no terminadas

--- ProyectoProgra2/src/main/java/module-info.java
+++ ProyectoProgra2/src/main/java/module-info.java
@@ -1,7 +1,14 @@
 module cr.ac.una.proyectoprogra2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
+    requires java.logging;
+    requires jakarta.persistence;
+    requires MaterialFX;
 
     opens cr.ac.una.proyectoprogra2 to javafx.fxml;
+    opens cr.ac.una.proyectoprogra2.util to javafx.fxml;
     exports cr.ac.una.proyectoprogra2;
+    exports cr.ac.una.proyectoprogra2.util;
+    exports cr.ac.una.proyectoprogra2.controller;
 }
