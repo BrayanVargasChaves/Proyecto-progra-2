73f14b01443c411ad90801c624abad4985007200
BrayanVargasChaves <141991258+BrayanVargasChaves@users.noreply.github.com>
2025-06-10 11:30:46 -0600

    Brayan Vargas Chaves
    Dificultades

--- ProyectoProgra2/src/main/java/cr/ac/una/proyectoprogra2/controller/DificultadController.java
+++ ProyectoProgra2/src/main/java/cr/ac/una/proyectoprogra2/controller/DificultadController.java
@@ -4,9 +4,16 @@
  */
 package cr.ac.una.proyectoprogra2.controller;
 
+import cr.ac.una.proyectoprogra2.model.Sonidos;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -15,12 +22,47 @@ import javafx.fxml.Initializable;
  */
 public class DificultadController implements Initializable {
 
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private ImageView ImvFondo;
+    @FXML
+    private MFXButton btnFacil;
+    @FXML
+    private MFXButton BtnNormal;
+    @FXML
+    private MFXButton btnDificil;
+    @FXML
+    private MFXButton btnVolver;
+    @FXML
+    private Label lblDificultad;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Sonidos.asignarSonido(btnFacil);
+        Sonidos.asignarSonido(BtnNormal);
+        Sonidos.asignarSonido(btnDificil);
+        Sonidos.asignarSonido(btnVolver);
     }    
+
+    @FXML
+    private void onActionBtnFacil(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnNormal(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnDificil(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnVolver(ActionEvent event) {
+    }
     
 }
